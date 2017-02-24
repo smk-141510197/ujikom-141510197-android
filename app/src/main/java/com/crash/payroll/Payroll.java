3e2e834bef643d4c6ef0ce511ab6b67218ab5319
fathurrahmanhardi <141510197@smkassalaambandung.sch.id>
2017-02-24 14:05:58 +0700

    Hari Keempat (2):

---
+++ app/src/main/java/com/crash/payroll/Payroll.java
@@ -0,0 +1,13 @@
+package com.crash.payroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payroll);
+    }
+}
